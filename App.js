0b91a19cda3c69ef749f9d4b6416ec363500c3da
Yoojin Lee <80568037+yoojinlee-hub@users.noreply.github.com>
2022-01-11 16:12:12 +0900

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import KoBay from './Hello';
+
+function App() {
+  return (
+    <div className="App">
+      <KoBay/>
+    </div>
+  );
+}
+
+export default App;
